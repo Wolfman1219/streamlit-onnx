add12048e149aa25aa41083576db2151bc542e80
Abduraxim <ajbborov1234@gmail.com>
2023-08-21 11:51:25 -0400

    message

--- app.py
+++ app.py
@@ -1,196 +1,113 @@
-import glob
-import streamlit as st
-import wget
-from PIL import Image
-import torch
 import cv2
-import os
-import time
+import numpy as np
+import torch
+from RT.models.utils import blob
+from RT.models.torch_utils import det_postprocess
+from RT.models.cudart_api import TRTEngine
+from RT.models import EngineBuilder
+import streamlit as st
+import tempfile
+from PIL import Image
 
-st.set_page_config(layout="wide")
+class Deployment:
+    def __init__(self):
+        self.model = "models/yolov8n.engine"
+        self.colors = [(255,0,0),(0,0,255),(0,0,0)]
+        
+    def ImageBox(self, image):
+        
+        new_shape=(640, 640)
+        width, height, channel = image.shape
+        
+        ratio = min(new_shape[0] / width, new_shape[1] / height)
+        new_unpad = int(round(height * ratio)), int(round(width * ratio))
+        dw, dh = (new_shape[0] - new_unpad[0])/2, (new_shape[1] - new_unpad[1])/2
 
-cfg_model_path = 'models/yolov5s.pt'
-model = None
-confidence = .25
+        if (height, width) != new_unpad:
+            image = cv2.resize(image, new_unpad, interpolation=cv2.INTER_LINEAR)
+        
+        top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+        left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+        
+        image = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value = self.colors[-1])
+        
+        return image, ratio, (dw, dh)
 
+    def face_detection_for_image_with_engine(self, image):
+      
+        model = TRTEngine(self.model)
+        
+        img, ratio, dwdh = self.ImageBox(image)
+        tensor = blob(img, return_seg=False)
+        tensor = torch.asarray(tensor)
 
-def image_input(data_src):
-    img_file = None
-    if data_src == 'Sample data':
-        # get all sample images
-        img_path = glob.glob('data/sample_images/*')
-        img_slider = st.slider("Select a test image.", min_value=1, max_value=len(img_path), step=1)
-        img_file = img_path[img_slider - 1]
-    else:
-        img_bytes = st.sidebar.file_uploader("Upload an image", type=['png', 'jpeg', 'jpg'])
-        if img_bytes:
-            img_file = "data/uploaded_data/upload." + img_bytes.name.split('.')[-1]
-            Image.open(img_bytes).save(img_file)
+        dwdh = np.array(dwdh * 2, dtype=np.float32)
 
-    if img_file:
-        col1, col2 = st.columns(2)
-        with col1:
-            st.image(img_file, caption="Selected Image")
-        with col2:
-            img = infer_image(img_file)
-            st.image(img, caption="Model prediction")
+        results = model(tensor)
 
+        bboxes, scores, labels = det_postprocess(results)
+        bboxes = (bboxes-dwdh)/ratio
+        
+        for (bbox, score, label) in zip(bboxes, scores, labels):
+            bbox = bbox.round().astype(np.int32).tolist()
+            cv2.rectangle(image, (bbox[0],bbox[1]) , (bbox[2],bbox[3]) , self.colors[1], 2)
+    
+        return image
 
-def video_input(data_src):
-    vid_file = None
-    if data_src == 'Sample data':
-        vid_file = "data/sample_videos/sample.mp4"
-    else:
-        vid_bytes = st.sidebar.file_uploader("Upload a video", type=['mp4', 'mpv', 'avi'])
-        if vid_bytes:
-            vid_file = "data/uploaded_data/upload." + vid_bytes.name.split('.')[-1]
-            with open(vid_file, 'wb') as out:
-                out.write(vid_bytes.read())
+def run_tensorrt(image, enggine_path):
 
-    if vid_file:
-        cap = cv2.VideoCapture(vid_file)
-        custom_size = st.sidebar.checkbox("Custom frame size")
-        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        if custom_size:
-            width = st.sidebar.number_input("Width", min_value=120, step=20, value=width)
-            height = st.sidebar.number_input("Height", min_value=120, step=20, value=height)
+    deployment = Deployment()    
 
-        fps = 0
-        st1, st2, st3 = st.columns(3)
-        with st1:
-            st.markdown("## Height")
-            st1_text = st.markdown(f"{height}")
-        with st2:
-            st.markdown("## Width")
-            st2_text = st.markdown(f"{width}")
-        with st3:
-            st.markdown("## FPS")
-            st3_text = st.markdown(f"{fps}")
-
-        st.markdown("---")
-        output = st.empty()
-        prev_time = 0
-        curr_time = 0
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                st.write("Can't read frame, stream ended? Exiting ....")
-                break
-            frame = cv2.resize(frame, (width, height))
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            output_img = infer_image(frame)
-            output.image(output_img)
-            curr_time = time.time()
-            fps = 1 / (curr_time - prev_time)
-            prev_time = curr_time
-            st1_text.markdown(f"**{height}**")
-            st2_text.markdown(f"**{width}**")
-            st3_text.markdown(f"**{fps:.2f}**")
-
-        cap.release()
+    predicted_image = deployment.face_detection_for_image_with_engine(image)
+    return predicted_image
 
 
-def infer_image(img, size=None):
-    model.conf = confidence
-    result = model(img, size=size) if size else model(img)
-    result.render()
-    image = Image.fromarray(result.ims[0])
-    return image
-
-
-@st.experimental_singleton
-def load_model(path, device):
-    model_ = torch.hub.load('ultralytics/yolov5', 'custom', path=path, force_reload=True)
-    model_.to(device)
-    print("model to ", device)
-    return model_
-
-
-@st.experimental_singleton
-def download_model(url):
-    model_file = wget.download(url, out="models")
-    return model_file
-
-
-def get_user_model():
-    model_src = st.sidebar.radio("Model source", ["file upload", "url"])
-    model_file = None
-    if model_src == "file upload":
-        model_bytes = st.sidebar.file_uploader("Upload a model file", type=['pt'])
-        if model_bytes:
-            model_file = "models/uploaded_" + model_bytes.name
-            with open(model_file, 'wb') as out:
-                out.write(model_bytes.read())
-    else:
-        url = st.sidebar.text_input("model url")
-        if url:
-            model_file_ = download_model(url)
-            if model_file_.split(".")[-1] == "pt":
-                model_file = model_file_
-
-    return model_file
 
 def main():
-    # global variables
-    global model, confidence, cfg_model_path
+    DEFAULT_VIDEO_PATH = "/home/hasan/Public/yolo_with_streamlit/data/sample_videos/sample.mp4"
 
-    st.title("Object Recognition Dashboard")
+# Create a video file uploader
+    st.header("Upload a video for inference")
+    uploaded_file = st.file_uploader("Choose a video...", type=["mp4", "avi", "mov"])
 
-    st.sidebar.title("Settings")
+    # Create a radio button for selecting between default video and uploaded video
+    video_selection = st.radio(
+        "Select video for inference:",
+        ("Use default video", "Use uploaded video")
+    )
 
-    # upload model
-    model_src = st.sidebar.radio("Select yolov5 weight file", ["Use our demo model 5s", "Use your own model"])
-    # URL, upload file (max 200 mb)
-    if model_src == "Use your own model":
-        user_model_path = get_user_model()
-        if user_model_path:
-            cfg_model_path = user_model_path
+    # If the user chooses to use the default video
+    if video_selection == "Use default video":
+        video_path = DEFAULT_VIDEO_PATH
 
-        st.sidebar.text(cfg_model_path.split("/")[-1])
-        st.sidebar.markdown("---")
+    # If the user chooses to use the uploaded video
+    elif video_selection == "Use uploaded video" and uploaded_file is not None:
+        tfile = tempfile.NamedTemporaryFile(delete=False) 
+        tfile.write(uploaded_file.read())
+        video_path = tfile.name
 
-    # check if model file is available
-    if not os.path.isfile(cfg_model_path):
-        st.warning("Model file not available!!!, please added to the model folder.", icon="⚠️")
+    # If there's a video to process, do the inference
+    if video_path is not None:
+        # Load the video with cv2
+        cap = cv2.VideoCapture(video_path)
+        outputing = st.empty()
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # Run the inference
+            output = run_tensorrt(image=frame, enggine_path='models/yolov8n.engine')
+
+            # Convert the output to an image that can be displayed
+            output_image = Image.fromarray(output)
+
+            # Display the image
+            outputing.image(output_image)
+
+        cap.release()
     else:
-        # device options
-        if torch.cuda.is_available():
-            device_option = st.sidebar.radio("Select Device", ['cpu', 'cuda'], disabled=False, index=0)
-        else:
-            device_option = st.sidebar.radio("Select Device", ['cpu', 'cuda'], disabled=True, index=0)
-
-        # load model
-        model = load_model(cfg_model_path, device_option)
-
-        # confidence slider
-        confidence = st.sidebar.slider('Confidence', min_value=0.1, max_value=1.0, value=.45)
-
-        # custom classes
-        if st.sidebar.checkbox("Custom Classes"):
-            model_names = list(model.names.values())
-            assigned_class = st.sidebar.multiselect("Select Classes", model_names, default=[model_names[0]])
-            classes = [model_names.index(name) for name in assigned_class]
-            model.classes = classes
-        else:
-            model.classes = list(model.names.keys())
-
-        st.sidebar.markdown("---")
-
-        # input options
-        input_option = st.sidebar.radio("Select input type: ", ['image', 'video'])
-
-        # input src option
-        data_src = st.sidebar.radio("Select input source: ", ['Sample data', 'Upload your own data'])
-
-        if input_option == 'image':
-            image_input(data_src)
-        else:
-            video_input(data_src)
-
+        st.write("Please upload a video file or choose to use the default video.")
 
 if __name__ == "__main__":
-    try:
-        main()
-    except SystemExit:
-        pass
+    main()
